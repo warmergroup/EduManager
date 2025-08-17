ca8e611301d4a91c8746c9a5a527054f5a030600
warmergroup <ulugbekjonsultonov4@gmail.com>
2025-08-17 18:40:19 +0500

    update frontend/vercel.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,10 +5,24 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),tailwindcss()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        format: 'es',
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  },
+  server: {
+    port: 3000
   }
 })
